eb00ba03b883797f546192f8caf6bcfedee922ea
Torres <david_ame2@hotmail.com>
2014-06-27 16:22:23 -0500

    primer commit

---
+++ practica1/guardar.php
@@ -0,0 +1,20 @@
+<?php
+echo "hola...";
+echo "Goles Equipo Local :" . $_POST['equipo1_local'];
+echo "<br />";
+echo "Goles Equipo Visitante :" . $_POST['equipo2_visitante'];
+echo "<br />";
+echo "Goles Equipo 2 Visitante: " . $_POST['equipo2_visitante']; 
+echo "<br />";
+echo "Goles Equipo 3 Local: " . $_POST['equipo3_local'];
+echo "<br />";
+echo "Goles Equipo 4 Visitante: " . $_POST['equipo4_visitante'];
+echo "<br />";
+echo "Equipo Elegido 1 Local: " . $_POST['selectEquipo1_local'];
+echo "<br />";
+echo "Goles Equipo 5 Local: " . $_POST['equipo1_local'];
+echo "<br />";
+echo "Equipo Elegido 1 visitante: " . $_POST['selectEquipo2_local'];
+echo "<br />";
+echo "Goles Equipo 5 visitante: " . $_POST['equipo2_local'];
+?> 
